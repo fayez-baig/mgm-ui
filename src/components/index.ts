6e9c326a3a35f10e3fc0d6795b30393ebd2f0e3f
fayez-baig <coderninja16@gmail.com>
2020-10-05 01:31:47 +0530

    fix: exporting components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
-export {default as Button} from './Elements/Button/Button';
+export { default as Button } from './Elements/Button/Button';
+export { default as Input } from './Form/Input/Input';
+export { default as Row } from './Layout/Grid/Row/Row';
+export { default as Col } from './Layout/Grid/Col/Col';
