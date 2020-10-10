8ba37dd2c3414fc2418f304d2e359c1509c5fa80
fayez-baig <coderninja16@gmail.com>
2020-10-10 19:25:27 +0530

    fix: replaced p with Text component

--- src/components/Elements/Box/Box.stories.tsx
+++ src/components/Elements/Box/Box.stories.tsx
@@ -4,6 +4,7 @@ import Box from './Box';
 import Image from '../Image/Image';
 import { BoxProps } from './types';
 import { getStyles } from '../../styles/getStyles';
+import Text from './../Text/Text';
 
 export default {
 	argTypes: {
@@ -32,14 +33,14 @@ const defaultBoxTemplate: Story<BoxProps> = args => (
 			</div>
 			<div className={getStyles(['media-content'])}>
 				<div className={getStyles(['content'])}>
-					<p>
+					<Text>
 						<strong>John Smith</strong> <small>@johnsmith</small>{' '}
 						<small>31m</small>
 						<br />
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
 						efficitur sit amet massa fringilla egestas. Nullam condimentum
 						luctus turpis.
-					</p>
+					</Text>
 				</div>
 			</div>
 		</article>
