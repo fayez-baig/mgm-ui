ceed416422174ebee625c87fbc165a9d3864da52
fayez-baig <coderninja16@gmail.com>
2020-10-10 15:19:23 +0530

    fix: removed default props

--- src/components/Elements/Box/Box.tsx
+++ src/components/Elements/Box/Box.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { getStyles } from '../../styles/getStyles';
-import { BoxProps, defaultProps } from './types';
+import { BoxProps } from './types';
 
 const Box: React.FC<BoxProps> = ({ children }) => {
 	return <div className={getStyles(['box'])}>{children}</div>;
 };
 
-Box.defaultProps = defaultProps;
 export default Box;
