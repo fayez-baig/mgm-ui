59334f9ea60f60596eb3fd411e6e1b04efa01c18
fayez-baig <coderninja16@gmail.com>
2020-10-12 18:07:44 +0530

    fix: exporting components

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,10 +7,22 @@ export { default as Notification } from './Elements/Notification/Notification';
 export { default as DeleteButton } from './Elements/DeleteButton/DeleteButton';
 export { default as Text } from './Elements/Text/Text';
 export { default as Title } from './Elements/Title/Title';
-export { default as Tag } from './Elements/Tag/Tag';
 export { default as SubTitle } from './Elements/SubTitle/SubTitle';
-
 export { default as ProgressBar } from './Elements/ProgressBar/ProgressBar';
+
+//Tag
+export { default as Tag } from './Elements/Tag/Tag';
+export { default as AddonTag } from './Elements/Tag/AddonTag';
+export { default as TagWrapper } from './Elements/Tag/TagWrapper';
+
+//Table
+export { default as Table } from './Elements/Table/Table';
+export { default as Thead } from './Elements/Table/Thead';
+export { default as Tfoot } from './Elements/Table/Tfoot';
+export { default as Tbody } from './Elements/Table/Tbody';
+export { default as TableRow } from './Elements/Table/TableRow';
+export { default as TableHeading } from './Elements/Table/TableHeading';
+export { default as TableCell } from './Elements/Table/TableCell';
 
 //Form
 export { default as Input } from './Form/Input/Input';
@@ -18,6 +30,7 @@ export { default as TextArea } from './Form/TextArea/TextArea';
 export { default as CheckBox } from './Form/CheckBox/CheckBox';
 export { default as Select } from './Form/Select/Select';
 export { default as Radio } from './Form/Radio/Radio';
+export { default as LoadingWrapper } from './Form/LoadingWrapper/LoadingWrapper';
 
 //Layout
 export { default as Row } from './Layout/Grid/Row/Row';
